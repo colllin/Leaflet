ca2f6ef1bcc60104ac5b72a9844fe91e4bcb0d2f
colllin <collindo@gmail.com>
2017-07-28 14:08:10 -0600

    Fix .gitignore for .next/; More work on <Map>.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CRS from '../geo/crs/CRS';
+import {EPSG3857} from '../geo/crs/CRS.EPSG3857';
 
 export default class extends React.Component {
     static propTypes = {
         crs: PropTypes.object.isRequired,
     };
     static defaultProps = {
-
+        crs: EPSG3857,
     };
 
     render() {
